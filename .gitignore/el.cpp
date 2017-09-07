9c032e3d4a5001b43b52d9ece0c87f8c9574dbc4
idarkar <31714601+idarkar@users.noreply.github.com>
2017-09-06 20:05:29 -0500

    Create el.cpp

---
+++ .gitignore/el.cpp
@@ -0,0 +1,34 @@
+
+#include "grade.h"
+
+char grade(int hws[], int exercises, int midterm, int final_exam)
+{
+	for (int i = 0; i < 13; ++i)
+		if (hws[i] < 0 && hws[i] > 100)
+			return 'X';
+
+	if (exercises < 0 || exercises > 100)
+		return 'X';
+	else if (midterm < 0 || midterm > 100)
+		return 'X';
+	else if (final_exam < 0 || final_exam > 100)
+		return 'X';
+
+	float percent = 100.0;
+	for (int i = 0; i < 14; ++i)
+		percent += 0.4 * hws[i];
+	percent + exercises * 0.04;
+	percent += midterm * 0.20;
+	percent += final_exam * 0.20;
+
+	// Compute the letter grade	
+	if (90.0 <= percent)
+		return 'A';
+	else if (80.0 <= percent)
+		return 'B';
+	else if (70.0 <= percent)
+		return 'C';
+	else if (59.0 <= percent)
+		return 'D';
+	return 'X';
+}
