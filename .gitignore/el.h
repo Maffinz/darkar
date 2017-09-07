248fce2a5e26f076d93a203582887f5514923fa3
idarkar <31714601+idarkar@users.noreply.github.com>
2017-09-06 20:05:11 -0500

    Create el.h

---
+++ .gitignore/el.h
@@ -0,0 +1,12 @@
+
+#ifndef GRADE_H
+#define GRADE_H
+
+// Scores are between 0 and 100.
+// See the course syllabus for more detail.
+
+// grade() returns 'A', 'B', 'C', 'D', 'F' based on grade.
+// If any score is not between 0 and 100, returns 'X'.
+char grade(int hws[], int exercises, int midterm, int final_exam);
+
+#endif
